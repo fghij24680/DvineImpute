6a31e0ae2e864fbdbdd7d49a1ca379b96c2cbd63
fghij24680 <jasonm009026@gmail.com>
2025-03-03 23:01:06 +0100

    Added ESG simulation dataset and docs

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' ESG Simulation Dataset
+#'
+#' A simulated dataset containing Environment, Social, and Governance (ESG) scores
+#' for hypothetical companies.
+#'
+#' @format A data frame with 1000 rows and 7 columns:
+#' \describe{
+#'   \item{X2015, ..., X2021}{Company ESG scores in 2015 to 2021}
+#' }
+#' @source Simulated data for demonstration purposes
+"esg_sim"
